865fdcd2a69482febabf83a1db22fc252f96d58f
tiswan14 <tiswan30@gmail.com>
2023-05-07 19:28:21 +0700

    first

---
+++ js/main.js
@@ -0,0 +1,14 @@
+const menuBar = document.querySelector(".menu-bar");
+const menuNav = document.querySelector(".menu");
+
+menuBar.addEventListener("click", () => {
+  menuNav.classList.toggle("menu-active");
+});
+
+// Ketika kita scrool akan menambahkan bg
+const navBar = document.querySelector(".navbar");
+
+window.addEventListener("scroll", () => {
+  const windowPosition = window.scrollY > 0;
+  navBar.classList.toggle("scrolling-active", windowPosition);
+});
